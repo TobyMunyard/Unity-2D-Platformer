a9eee19f8fafb4e1adc10be6e9930ee88b5ae162
TobyMunyard <tobymunyard@gmail.com>
2025-07-10 10:56:41 +1200

    Added Jumping
    * Added player jumping to the PlayerMove script.

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -5,10 +5,13 @@ public class PlayerMove : MonoBehaviour
 {
     private Rigidbody2D rb;
 
-    [SerializeField] private float moveSpeed = 5;
-    [SerializeField] private float sprintSpeed = 8;
+    [SerializeField] private float moveSpeed;
 
     float horizontalMovement;
+    float jumpingPower = 8f;
+
+    public LayerMask groundLayer;
+    public Transform groundCheck;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,5 +28,18 @@ public class PlayerMove : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (IsGrounded() && context.performed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, 0.1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
     }
 }
